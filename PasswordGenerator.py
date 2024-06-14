a99d3e4763ed53baa8f2df9c4281e3649b652239
anastaciaintech <158435153+anastaciaintech@users.noreply.github.com>
2024-06-14 21:09:30 +0200

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,73 @@
+import validate_password
+import random
+
+
+
+
+"""
+Create a random password generator. 
+It must be a command line password generator.
+Must be based on user defined criteria such as lengths and character types(letters, numbers, symbols)
+Allow users to set character lengths and character set preferences.
+Add an eye button next to the password.
+Create a text box for the password input. 
+Consider the User Interface.
+"""
+
+# would a list work in this instance
+
+#small_letters = "abcdefghijklmnopqrstuvwxyz"  # user can input upper and lower case letters
+#big_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#symbols = "$%&#"
+#num= "0123456789"
+
+#password = "ABCD"
+#user_password = input(password)
+
+#if "A" in user_password:
+    #print("Password Correct!")
+#elif "B" in user_password:
+    #print("Password Correct!")
+#elif "C" in user_password:
+    #print("Password Correct!")
+#elif "D" in user_password:
+    #print("Password Correct!")
+#else:
+    #print("Password Incorrect!")
+
+
+
+password =input()
+
+def validate_password(password):
+    
+    (length >= 6
+    uppercase = 1
+    lowercase = 1
+    symbol = 1)
+ return validate_password(password)
+
+if return
+
+
+
+if password > len :
+    print("Password Incorrect!")
+else:
+    print("Password Incorrect!")
+
+
+#if small_letters in password:
+    print("Valid password!")
+#elif big_letters in password:
+    print("Valid password!")
+#elif symbols in password:
+    print("Valid password!")
+#elif num in password:
+    print("Valid password")
+#else:
+    print("Invalid password")
+
+
+
+
